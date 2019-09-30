3b5f1d89cfc3473c07d6fcae81e6b7bcbf54420f
Adam Jaamour <adam@jamour.com>
2019-09-30 19:47:36 +0100

    Initial commit

---
+++ src/A1Main.java
@@ -0,0 +1,20 @@
+public class A1Main {
+
+    public static void main(String[] args) {
+
+        // step 1: parse input
+        // N
+        // start point
+        // end point
+        //search type
+
+        // step 2: save into a data structure called [Problem]
+
+        // step 3: represent world
+
+        // step 4: initialise search algorithm with start/goal and algorithm
+
+        // step 5: start the search
+
+    }
+}
